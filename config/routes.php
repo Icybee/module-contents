787997ad8933e73aa39968efc79d4a0a60bf9c3d
Olivier Laviale <contact@weirdog.com>
2013-06-12 10:31:30 +0200

    Remove references to _operation*

--- config/routes.php
+++ config/routes.php
@@ -2,18 +2,20 @@
 
 namespace Icybee\Modules\Contents;
 
+use ICanBoogie\Operation;
+
 return array
 (
 	'api:contents:is-home-excluded:set' => array
 	(
-		'pattern' => '/api/:_operation_module/<_operation_key:\d+>/is-home-excluded',
+		'pattern' => '/api/:' . Operation::DESTINATION . '/<' . Operation::KEY . ':\d+>/is-home-excluded',
 		'controller' => __NAMESPACE__ . '\HomeExcludeOperation',
 		'via' => 'PUT'
 	),
 
 	'api:contents:is-home-excluded:unset' => array
 	(
-		'pattern' => '/api/:_operation_module/<_operation_key:\d+>/is-home-excluded',
+		'pattern' => '/api/:' . Operation::DESTINATION . '/<' . Operation::KEY . ':\d+>/is-home-excluded',
 		'controller' => __NAMESPACE__ . '\HomeIncludeOperation',
 		'via' => 'DELETE'
 	)
